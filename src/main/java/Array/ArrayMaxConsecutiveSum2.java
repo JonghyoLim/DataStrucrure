e13a8336bbf85adee65d779acb5c7d678b7161f6
JonghyoLim <hyokun07@gmail.com>
2021-03-18 12:07:51 +0000

    testing another case

--- src/main/java/Array/ArrayMaxConsecutiveSum2.java
+++ src/main/java/Array/ArrayMaxConsecutiveSum2.java
@@ -9,16 +9,17 @@ public class ArrayMaxConsecutiveSum2 {
        // a = [-2, 2, 5, -11, 6] == 7 at least contains 1 elment 
        //max_sum = interger.MIN_VALUE , current_sum
        //int[] array = {-2, 2, 5, -11, 6};
-       int[] array = {-7, -1, 10, 11, -3};
+       //int[] array = {-7, -1, 10, 11, -3};
+       int[] array = {3, -4, 21, -18, 15};
         System.out.println("Max Consecutive sum: " +arrayMaxConsecutiveSum2(array));
     }
     
     static int arrayMaxConsecutiveSum2(int[] arr) {
-        int max_sum = arr[0]; // -7 
-        int current_sum = max_sum; // -7         
+        int max_sum = arr[0]; // //3
+        int current_sum = max_sum; // 3         
         for (int i = 1; i < arr.length; i++) {
-            current_sum = Math.max(arr[i] + current_sum, arr[i]); //-1, 10, 21, 18
-            max_sum = Math.max(current_sum, max_sum); //-1, 9, 21, 21
+            current_sum = Math.max(arr[i] + current_sum, arr[i]); //-1 21 3 18
+            max_sum = Math.max(current_sum, max_sum); //3 21 21 21
         }
         return max_sum; //21
     }
