a390aa713374c638f83ec3e8ccbf3bc4f61759e2
JonghyoLim <hyokun07@gmail.com>
2021-03-23 09:34:20 +0000

    Array - Contains Duplicated Numbers (HashSet)

---
+++ src/main/java/Array/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+
+package Array;
+
+import java.util.*;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] a1 = {1,2,3,1};
+        int[] a2 = {1,2,3,4};
+        int[] a3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println("Array 1: " + containsDuplicate(a1));
+        System.out.println("Array 2: " + containsDuplicate(a2));
+        System.out.println("Array 3: " + containsDuplicate(a3));
+    }
+    
+    static boolean containsDuplicate(int[] arr) {
+        HashSet<Integer> numbers = new HashSet<Integer>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (numbers.contains(arr[i])) 
+                return true;
+            numbers.add(arr[i]);
+        }
+        return false;
+    }
+    
+}
