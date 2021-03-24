b00657f0d586c5d7aab5fa6ecc45e5fd9249b1c9
JonghyoLim <hyokun07@gmail.com>
2021-03-24 17:12:58 +0000

    Array - Stair case added new method

--- src/main/java/Array/StairCase.java
+++ src/main/java/Array/StairCase.java
@@ -12,10 +12,24 @@ public class StairCase {
         _ # # #      1           3
         # # # #      0           4
        */  
-        stairCase(4);
+        stairCaseV1(7);
+//        stairCaseV2(4);
     }
     
-    static void stairCase(int n) {
+    static void stairCaseV1(int n) {
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                if (j < n - i - 1){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    
+    static void stairCaseV2(int n) {
         for (int numSymbols = 1; numSymbols <= n; numSymbols++) {
             int numSpaces = n - numSymbols;
             
