608769808fc9c9bd6b28b8d500a2446c2d978528
JonghyoLim <hyokun07@gmail.com>
2021-03-25 08:33:13 +0000

    Array - Staircase added new methods

--- src/main/java/Array/StairCase.java
+++ src/main/java/Array/StairCase.java
@@ -12,9 +12,38 @@ public class StairCase {
         _ # # #      1           3
         # # # #      0           4
        */  
-        stairCaseV1(7);
+        stairCaseV4(5);
+//        stairCaseV3(5);
+//        stairCaseV1(7);
 //        stairCaseV2(4);
     }
+    
+    static void stairCaseV4(int n) {
+       for (int i = 0; i < n; i++) {
+           for (int j = 0; j < n; j++) {
+               // j = 00, 10, 11 20, 21, 22, 30, 31, 32, 33 
+               if (j <= i) {
+                   System.out.print("#");
+               } else {
+                   System.out.print(" ");
+               }
+           }
+            System.out.println("");
+       } 
+    }
+    
+    static void stairCaseV3(int n) {
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j++) {
+                if (j <= n - 1 - i) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    } 
     
     static void stairCaseV1(int n) {
         for (int i = 0; i < n; i++) {
