00abea84c1dc2f6d18fbc30980631373554165c1
JonghyoLim <hyokun07@gmail.com>
2021-04-12 12:40:51 +0100

    Alternating Characters

--- src/main/java/String/AlternatingCharcters.java
+++ src/main/java/String/AlternatingCharcters.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package String;
 
-/**
- *
- * @author hyoku
- */
+import java.util.*;
+
 public class AlternatingCharcters {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int q = scanner.nextInt();
+        scanner.skip("\n");
+        for (int i = 0; i < q; i++) {
+            String s = scanner.nextLine();
+            int result = alternatingCharacters(s);
+            System.out.println(result);
+        }
+//        String[] s = new String[q];
+//  
+//        for (int i = 0; i < q; i++) {
+//            s[i] = scanner.nextLine();
+//        }
+//        for (int i = 0; i < s.length; i++){
+//            System.out.println(alternatingCharacters(s[i]));  
+//        }
+      
+       
     }
     
+    static int alternatingCharacters(String s){
+        //AABAAB ->ABAB
+        //remove [0], [3] => return 2;
+        int min_deletion = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) {
+                min_deletion++;
+            }
+        }
+        return min_deletion;
+    }
 }
