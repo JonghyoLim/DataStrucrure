9cf40afcaf864a3ff1c21a9e6f298d4efd092edd
JonghyoLim <hyokun07@gmail.com>
2021-04-12 13:19:50 +0100

    fixed Two Strings

--- src/main/java/HashMapDictionaries/TwoStrings.java
+++ src/main/java/HashMapDictionaries/TwoStrings.java
@@ -1,21 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package HashMapDictionaries;
 
-/**
- *
- * @author hyoku
- */
-public class TwoStrings {
+import java.util.*;
 
-    /**
-     * @param args the command line arguments
-     */
+public class TwoStrings {
+    
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < q; i++) {
+            String s1 = scanner.nextLine();
+            String s2 = scanner.nextLine();
+            String result = twoStrings(s1, s2);
+            System.out.println("Result: " + result);
+        }
+    }
+    
+    static String twoStrings(String s1, String s2) {
+        HashSet<Character> s1_chars = new HashSet();
+        HashSet<Character> s2_chars = new HashSet();
+        
+        for (int i = 0; i < s1.length(); i++) {
+            s1_chars.add(s1.charAt(i)); //h i
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            s2_chars.add(s2.charAt(i)); //e h i
+        }
+        
+        s1_chars.retainAll(s2_chars);//s1 only has 'h'
+        if (!s1_chars.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
     }
     
 }
