2a085ba5ea654c1de24af3ad4048e5bebea6b634
JonghyoLim <hyokun07@gmail.com>
2021-04-15 13:19:24 +0100

    changed method

--- src/main/java/String/LengthOfLongestSubstring.java
+++ src/main/java/String/LengthOfLongestSubstring.java
@@ -4,7 +4,24 @@ package String;
 import java.util.*;
 
 public class LengthOfLongestSubstring {
-
+    
+    static int lengthOfLongestSubstring(String s){
+        HashSet<Character> char_set = new HashSet<Character>();
+        //pointers
+        int a_pointer = 0, b_pointer = 0, max = 0;
+        //a b c a b c b b
+        while (b_pointer < s.length()) {
+            if (!char_set.contains(s.charAt(b_pointer))) {
+                char_set.add(s.charAt(b_pointer));//abc- abc 
+                b_pointer++;//1,2,3,4
+                max = Math.max(char_set.size(), max);//1,2,3
+            } else  {
+                char_set.remove(s.charAt(a_pointer));//abc,bc- abc,ac,abc
+                a_pointer++;//1
+            }
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
         String s1 = "abcabcbb";
@@ -16,21 +33,5 @@ public class LengthOfLongestSubstring {
     
     }
     
-    static int lengthOfLongestSubstring(String s){
-        //pointers
-        int a_pointer = 0, b_pointer = 0, max = 0;
-        HashSet<Character> char_set = new HashSet<Character>();
-        
-        while (b_pointer < s.length()) {
-            if (!char_set.contains(s.charAt(b_pointer))) {
-                char_set.add(s.charAt(b_pointer));
-                b_pointer++;
-                max = Math.max(char_set.size(), max);
-            } else  {
-                char_set.remove(s.charAt(a_pointer));
-                a_pointer++;
-            }
-        }
-        return max;
-    }
+
 }
