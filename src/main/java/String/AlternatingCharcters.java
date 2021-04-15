a0610eaf72c3c4bcdb93ed4a1679e0aa13480597
JonghyoLim <hyokun07@gmail.com>
2021-04-15 08:28:15 +0100

    added comments

--- src/main/java/String/AlternatingCharcters.java
+++ src/main/java/String/AlternatingCharcters.java
@@ -7,30 +7,9 @@ public class AlternatingCharcters {
 
     private static final Scanner scanner = new Scanner(System.in);
     
-    public static void main(String[] args) {
-
-        int q = scanner.nextInt();
-        scanner.skip("\n");
-        for (int i = 0; i < q; i++) {
-            String s = scanner.nextLine();
-            int result = alternatingCharacters(s);
-            System.out.println(result);
-        }
-//        String[] s = new String[q];
-//  
-//        for (int i = 0; i < q; i++) {
-//            s[i] = scanner.nextLine();
-//        }
-//        for (int i = 0; i < s.length; i++){
-//            System.out.println(alternatingCharacters(s[i]));  
-//        }
-      
-       
-    }
-    
     static int alternatingCharacters(String s){
-        //AABAAB ->ABAB
-        //remove [0], [3] => return 2;
+        //AABAAB - ABAB remove [0],[3]=>return 2
+        //AAAABBBBBAB -[0][1][2][4][5][6][7]=>return 7
         int min_deletion = 0;
         for (int i = 0; i < s.length()-1; i++) {
             if (s.charAt(i) == s.charAt(i+1)) {
@@ -39,4 +18,16 @@ public class AlternatingCharcters {
         }
         return min_deletion;
     }
+    
+    public static void main(String[] args) {
+
+        int q = scanner.nextInt();
+        scanner.skip("\n");
+        for (int i = 0; i < q; i++) {
+            String s = scanner.nextLine();
+            System.out.println(alternatingCharacters(s));
+        }   
+    }
+    
+
 }
