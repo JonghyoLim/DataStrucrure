c03d74d50a971efc5f64cbe8c5f25cc877a4d120
JonghyoLim <hyokun07@gmail.com>
2021-04-15 18:30:09 +0100

    changed the test cases

--- src/main/java/String/ReverseInteger.java
+++ src/main/java/String/ReverseInteger.java
@@ -4,27 +4,29 @@ package String;
 import java.util.*;
 
 public class ReverseInteger {
-
-    public static void main(String[] args) {
-        
-        int n = 1534236469; //9,646,324,351 = 0
-        //int n = 1534236461;//1646324351
-        System.out.println(reverseNum(n)); //9,646,324,351 = 0
-    }
     
     static int reverseNum(int x) {
         //Integer.MAX_VALUE =  2,147,483,647
         //Integer.MIN_VALUE = -2,147,483,648
-        int result = 0;//9,646,324,35
+        //x = 1,646,324,351
+        int result = 0;
         while (x != 0) {
-            int lastDigit = x % 10;
-            int newResult = result * 10 + lastDigit; //10,646,324,351
+            int lastDigit = x % 10;//1
+            int newResult = result * 10 + lastDigit; //1
             if ((newResult - lastDigit) / 10 != result) {
                 return 0;
             }
             x /= 10;
             result = newResult;
         }
-        return result;
+        return result;//9,646,324,35
     }
+    
+    public static void main(String[] args) {
+        
+        //int n = 1,534,236,469; // return 9,646,324,351 = 0
+        int n = 1534236461;//1,646,324,351
+        System.out.println(reverseNum(n)); //9,646,324,351 = 0
+    }
+
 }
