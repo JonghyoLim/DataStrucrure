4060347c55252a3cd960128fc1adc788ad12e4df
JonghyoLim <hyokun07@gmail.com>
2021-04-15 13:55:32 +0100

    changed method

--- src/main/java/String/LongestConsecutiveChar.java
+++ src/main/java/String/LongestConsecutiveChar.java
@@ -4,35 +4,35 @@ package String;
 import java.util.*;
 
 public class LongestConsecutiveChar {
-    //current 'B'
-    //current_count 1
-    //prev_char 'A'
-    //max_char  'A'
-    //max_count 2
-    public static HashMap<Character, Integer> longestConsChar(String str){
-        //create empty HashMap
-        HashMap<Character, Integer> lcChar = new HashMap<Character, Integer>();
+    //current_char A,B,C,C,C,B,C,B,A
+    //current_count 1,2,3,1,1,1,1
+    //max_char  A,B,C
+    //max_count O,1,1,1,2,3
+    //prev_char A,B,C,C,B,C,B,A
+    public static HashMap<Character, Integer> longestConsChar(String s){
+        //s = A B C C C B C B A
+        HashMap<Character, Integer> longestChar = new HashMap<>();
         int max_count = 0, current_count = 0;
         char current_char = ' ', prev_char = ' ', max_char = ' ';
-        for (int i = 0; i < str.length(); i++) {
-            current_char = str.charAt(i); 
+        for (int i = 0; i < s.length(); i++) {
+            current_char = s.charAt(i);//A,B
             if (prev_char == current_char) current_count += 1;
-            else current_count = 1;
+            else current_count = 1;//1,1
             
-            if (current_count > max_count){
-                max_count = current_count;
-                max_char = current_char;
+            if (current_count > max_count){ //1 > 0
+                max_char = current_char;//max_char:A
+                max_count = current_count;//max_count:1
             }
-            prev_char = current_char;
+            prev_char = current_char;//prev_char:A,B,C
         }
-        lcChar.put(max_char, max_count); 
-        return lcChar; // {'B' : 3}
+        longestChar.put(max_char, max_count);//A=1
+        return longestChar; // {C = 3}
     }  
 
     public static void main(String[] args) {
        
-        String str= "ABCCCCBCDDBBBEA";
-        System.out.println("Result: " + longestConsChar(str)); //{C=4}
+        String str= "ABCCCBCBA";
+        System.out.println("Result: " + longestConsChar(str)); //{C=3}
     }
     
 }
