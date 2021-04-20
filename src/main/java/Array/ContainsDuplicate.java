995eebc63fa09800a8e865f4287543aefca3c0f3
JonghyoLim <hyokun07@gmail.com>
2021-04-20 10:24:05 +0100

    Used HashSet

--- src/main/java/Array/ContainsDuplicate.java
+++ src/main/java/Array/ContainsDuplicate.java
@@ -9,9 +9,9 @@ public class ContainsDuplicate {
         int[] a1 = {1,2,3,1};
         int[] a2 = {1,2,3,4};
         int[] a3 = {1,1,1,3,3,4,3,2,4,2};
-        System.out.println("Array 1: " + containsDuplicate(a1));
-        System.out.println("Array 2: " + containsDuplicate(a2));
-        System.out.println("Array 3: " + containsDuplicate(a3));
+        System.out.println("Array 1: " + containsDuplicate(a1));//t
+        System.out.println("Array 2: " + containsDuplicate(a2));//f
+        System.out.println("Array 3: " + containsDuplicate(a3));//t
     }
     
     static boolean containsDuplicate(int[] arr) {
