ad1fc42275311db1722787261628839f0cc4741f
JonghyoLim <hyokun07@gmail.com>
2021-04-20 10:16:42 +0100

    changed the method

--- src/main/java/Array/FindCelebrity.java
+++ src/main/java/Array/FindCelebrity.java
@@ -4,7 +4,7 @@ package Array;
 import java.util.*;
 
 public class FindCelebrity {
-
+    //SOTI testing
     public static void main(String[] args) {
         //Time O(N)
     int[][] M = {{0, 1, 1, 0, 1}, 
@@ -12,24 +12,24 @@ public class FindCelebrity {
                  {1, 1, 0, 1, 1},
                  {1, 0, 1, 0, 1},
                  {0, 0, 0, 0, 0}};
-    System.out.println("Celebrity ID: " + FindCelebrityID(M, 4));      
+    System.out.println("Celebrity ID: " + FindCelebrityID(M, 5));      
     }
     
     static String FindCelebrityID(int[][] M, int n) {
-      //finding celebrity 
+      //finding celebrity: all ppl knows celebrity
       int a = 0, b = n - 1;  
       String foundID = ""; 
       while(a < b) {
-          if (M[a][b] == 1) // [0][2] //[1][2]
-              a++; //1 -> 2
-          else if (M[a][b] == 0) //[0][3] 
-              b--; //2
+          if (M[a][b] == 1)//[0][4]//[1][4]...[3][4]
+              a++;//1,2,3,4
+          else if (M[a][b] == 0) 
+              b--; 
       }  
       //Check celebrity conditions 
       //if a is celebrity then all members has to know a
       //a does not know anybody
       for (int i = 0; i < n; i++) { 
-          // M[a][i] = [0 0 0 0], M[i][a] = col[1, 1, 0, 1]
+          // M[a][i] = [0 0 0 0 0], M[i][a] = [1, 1, 1, 1, 0]
           if ((i != a) && (M[a][i] == 1 || M[i][a] == 0)) 
               return "No Celebrity found";
       }
@@ -37,9 +37,9 @@ public class FindCelebrity {
         case 0: foundID = "A"; break;
         case 1: foundID = "B"; break;
         case 2: foundID = "C"; break;
-        case 3: foundID = "d"; break;
-        case 4: foundID = "e"; break;
-        case 5: foundID = "f"; break;   
+        case 3: foundID = "D"; break;
+        case 4: foundID = "E"; break;
+        case 5: foundID = "F"; break;   
       }
       return foundID;
     }
